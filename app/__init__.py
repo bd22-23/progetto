4ae246b423d50eaf9ffd87688a650367e0588cc3
Simon-Scoltock <simon.scoltock.work@gmail.com>
2023-06-06 17:26:40 +0200

    initial changes to pages

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Development')
+    app.config.from_object('config.Production')
     db.init_app(app)
 
     with app.app_context():
         db.create_all()
 
     # TODO: Register blueprints
+    from app.main.controller import main
+    app.register_blueprint(main)
 
     return app
