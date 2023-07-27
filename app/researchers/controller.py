3ec2e0e073a4ed58d13f626b4a6bafcecc398b65
Simon Scoltock <simon.scoltock.work@gmail.com>
2023-07-27 16:21:20 +0200

    changed edit profile for researcher (with wtf form)

--- app/researchers/controller.py
+++ app/researchers/controller.py
@@ -4,19 +4,17 @@ from app.auth import User
 from app.researchers import Researcher
 
 from app import db
+from app.researchers.forms import EditResearcherForm
 
 researcher = Blueprint('researcher', __name__, url_prefix='/researcher', template_folder='templates')
 
 
-@researcher.route('/profile/<profile_id>')
+@researcher.route('/profile/<profile_id>', methods=['GET', 'POST'])
 def profile(profile_id):
     user = Researcher.query.join(User, User.id == Researcher.id).filter_by(id=profile_id).first()
-    return render_template("researcher_profile.html", user=user)
-
-
-@researcher.route('/profile/update/<profile_id>', methods=['POST'])
-def profile_update(profile_id):
-    res = Researcher.query.filter_by(id=profile_id).first()
-    res.update(db, request.form.get("name"), request.form.get("surname"), request.form.get("email"),
-               request.form.get("affiliation"), request.form.get("role"), request.form.get("pronouns"))
-    return redirect('/researcher/profile/' + str(profile_id))
+    form = EditResearcherForm(user)
+    if form.validate_on_submit():
+        user.update(db, form.name.data, form.surname.data, form.email.data, form.affiliation.data,
+                    form.role.data, form.pronouns.data)
+        return redirect('/researcher/profile/' + str(user.id))
+    return render_template("researcher_profile.html", user=user, form=form)
