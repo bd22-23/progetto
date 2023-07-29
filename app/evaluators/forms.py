dad45016eed0a5e9e0d2e97b9f268cc22bbe985a
Simon Scoltock <simon.scoltock.work@gmail.com>
2023-07-29 08:59:07 +0200

    added edit profile form for evaluators

--- app/evaluators/forms.py
+++ app/evaluators/forms.py
@@ -26,3 +26,40 @@ class NewProfileForm(FlaskForm):
         'Password'
     )
     submit = SubmitField('Crea')
+
+
+class EditProfileForm(FlaskForm):
+
+        name = StringField(
+            'Nome'
+        )
+        surname = StringField(
+            'Cognome'
+        )
+        bio = StringField(
+            'Bio'
+        )
+        pronouns = StringField(
+            'Pronomi'
+        )
+        email = StringField(
+            'Email',
+            validators=[Email("Inserisci un'email valida!")]
+        )
+        reset = SubmitField(
+            'Annulla',
+            render_kw={'class': 'mt-2 btn btn-sm btn-secondary js-editable-stop-edit-button', 'type': 'reset'}
+        )
+        submit = SubmitField(
+            'Salva',
+            render_kw={'class': 'mt-2 btn btn-sm btn-primary'}
+        )
+
+        def __init__(self, user):
+            super().__init__()
+            if not self.validate_on_submit():
+                self.name.data = user.name
+                self.surname.data = user.surname
+                self.bio.data = user.bio
+                self.email.data = user.email
+                self.pronouns.data = user.pronouns
