a552dd92b26f2a4629886ad25e16f735a98d79b9
Paul <paultoffoloni@gmail.com>
2023-08-05 11:14:30 +0200

    Remove stupid menu and some PDFs

--- app/static/scripts/navbar.js
+++ app/static/scripts/navbar.js
@@ -1,16 +1,4 @@
 $(document).ready(function() {
-    $('.sub-nav-activator').hover(function() {
-        $(this).find('.sub-nav').first().slideDown("easeOutBounce");
-    }).mouseleave(function() {
-        if ($(this).find('.sub-nav').first().is(':hover')) {
-            $(this).find('.sub-nav').first().mouseleave(function() {
-                $(this).slideUp(500);
-            });
-        } else {
-            $(this).find('.sub-nav').first().slideUp(500);
-        }
-    });
-
     $('.js-edit-button').click(function () {
         $(this).closest(':has(.js-editable-info-area)').find('.js-editable-info-area').addClass('d-none');
         $('#' + $(this).attr('js-edit-area-id')).removeClass('d-none');
